8a14190fd5cc16d906ccb3e2f5d8e72030b6ee66
Nikhil Anand <nikhilanand2202@gmail.com>
2025-08-25 21:08:08 +0530

    setup completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser= asyncHandler(async(req,res)=>{
    return res.status(200).json({
-        message:"Chai aur Code"
+        message:"Ok"
     })
 })
 
