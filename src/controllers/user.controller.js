b2585ddabd09498a292381d74aadd1fc2c6f1d5f
Nikhil Anand <nikhilanand2202@gmail.com>
2025-09-03 09:56:32 +0530

    add changeCurrentPassword functionality to update user password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -224,7 +224,21 @@ try {
 }
 })
 
+const changeCurrentPassword= asyncHandler(async(req,res)=>{
+    const {oldPassword , newPassword}= req.body
+    const user=  await User.findById(req.user?.id)
+    const isPasswordCorrect= await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid Old Password ")
 
+    }
+    user.password= newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password Changed Successfully"))
+})
 
 export {
     registerUser,
